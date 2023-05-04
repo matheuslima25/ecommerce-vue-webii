9b9650f83dd6649572051334ed24650011f754db
matheuslima25 <josematheuslima25@gmail.com>
2023-05-04 13:46:33 -0300

    feat: consuming from our api

--- app.js
+++ app.js
@@ -2,25 +2,31 @@ const app = Vue.createApp({
   // template: '<h1>Hello World</h1>',
   data() {
     return {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john@gmail.com',
-      gender: 'male',
-      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+      id: '3643aad7-6a82-4767-b52a-9542f1456439',
+      name: 'Keyboard',
+      description: 'Gamer Kayboard',
+      price: '59.99',
+      category: 'Peripheral',
+      picture: 'https://images.tcdn.com.br/img/img_prod/740836/teclado_mecanico_razer_blackwidow_v3_tenkeyless_razer_chroma_rgb_us_switch_yellow_12535_1_10e721931a23430dae9288fe66c122bb.jpg',
     }
   },
   methods: {
-    async getUser() {
-      const res = await fetch('https://randomuser.me/api')
-      const { results } = await res.json()
+    async getProduct() {
+      const res = await fetch('http://localhost:8080/api/products')
+      const data = await res.json()
+      const firstProduct = data[0]
 
-      // console.log(results)
+      console.log(data)
+      console.log(firstProduct)
 
-      this.firstName = results[0].name.first
-      this.lastName = results[0].name.last
-      this.email = results[0].email
-      this.gender = results[0].gender
-      this.picture = results[0].picture.large
+      this.id = firstProduct.id
+      this.name = firstProduct.name
+      this.description = firstProduct.description
+      this.price = firstProduct.price
+      this.category = firstProduct.category.name
+      this.image = 'http://localhost:8080' + firstProduct.image
+
+      console.log(this.image)
     },
   },
 })
